d32bc5ef999241ffa9c6d10d291344ff46e97768
lubianat <tiago.lubiana.alves@usp.br>
2021-04-30 11:00:28 -0300

    code: Add doi to qid function

--- tests/test_wbib.py
+++ tests/test_wbib.py
@@ -15,12 +15,20 @@ class TestWbib(unittest.TestCase):
         """Tear down test fixtures, if any."""
 
     def test_if_queries_run(self):
-        qids = ["Q35185544", "Q34555562", "Q21284234" ]
+        qids = ["Q35185544", "Q34555562", "Q21284234"]
         html = wbib.render_dashboard(qids)
         assert "Demonstration" in html
 
     def test_format_with_prefix(self):
-        qids = ["Q35185544", "Q34555562", "Q21284234" ]
+        qids = ["Q35185544", "Q34555562", "Q21284234"]
         result = "{ wd:Q35185544 wd:Q34555562 wd:Q21284234 }"
         test = wbib.format_with_prefix(qids)
-        assert  result == test
+        assert result == test
+
+    def test_doi_to_qid(self):
+        dois = ["10.3897/RIO.2.E9342", "10.3389/fimmu.2019.02736", "wrong"]
+        result = {"missing": set(["wrong"]), "qids": set(["Q61654697", "Q92072015"])}
+        test = wbib.convert_doi_to_qid(dois)
+        assert result["missing"] == test["missing"]
+        assert result["qids"] == test["qids"]
+
--- wbib/wbib.py
+++ wbib/wbib.py
@@ -3,14 +3,57 @@ import os
 import glob
 import urllib.parse
 import pandas as pd
-from wbib import queries 
+from wbib import queries
 import unicodedata
+from wikidata2df import wikidata2df
+
+
+def convert_doi_to_qid(list_of_dois):
+    """
+  Args:
+    dois: A list of dois without prefix (e.g. ["10.3897/RIO.2.E9342", "10.3389/fimmu.2019.02736"])
+
+  Returns:
+    result: A dict with two key-value pairs. The "missing" key contains a set with the DOIs not found, 
+    the "qids" key contain a set with the QIDs found on Wikidata. 
+  """
+    formatted_dois = '{ "' + '" "'.join(list_of_dois) + '"}'
+    query = f"""SELECT ?id ?item  ?itemLabel
+    WHERE {{
+        {{
+        SELECT ?item ?id WHERE {{
+            VALUES ?unformatted_id {formatted_dois}
+            BIND(UCASE(?unformatted_id) AS ?id)
+            ?item wdt:P356 ?id.
+        }}
+        }}
+        SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}}}
+    """
+
+    query_result = wikidata2df(query)
+
+    result = {}
+
+    result["qids"] = list(query_result["item"].values)
+    result["missing"] = []
+
+    for doi in list_of_dois:
+        if doi.upper() not in list(query_result["id"].values):
+            result["missing"].append(doi)
+
+    result["qids"] = set(result["qids"])
+    result["missing"] = set(result["missing"])
+    return result
+
 
 def format_with_prefix(list_of_qids):
     list_with_prefix = ["wd:" + i for i in list_of_qids]
-    return("{ " + " ".join(list_with_prefix) + " }")
+    return "{ " + " ".join(list_with_prefix) + " }"
 
-def render_dashboard(qids_for_articles, site_title = "Wikidata Bibtex", site_subtitle = "Demonstration"):
+
+def render_dashboard(
+    qids_for_articles, site_title="Wikidata Bibtex", site_subtitle="Demonstration"
+):
     """
     Renders the html for a default dashboard
 
@@ -29,34 +72,37 @@ def render_dashboard(qids_for_articles, site_title = "Wikidata Bibtex", site_sub
     url2_legend = "Topics of those articles"
     url4 = queries.get_query_url_for_venues(format_with_prefix(qids_for_articles))
     url4_legend = "Most common journals "
-    url5 = queries.get_query_url_for_authors(format_with_prefix(qids_for_articles)) 
+    url5 = queries.get_query_url_for_authors(format_with_prefix(qids_for_articles))
     url5_legend = "Authors of the works"
     url6 = queries.get_query_url_for_locations(format_with_prefix(qids_for_articles))
-    url6_legend =  "Map of institutions"
-    url7 =  queries.get_query_url_for_citing_authors(format_with_prefix(qids_for_articles))
+    url6_legend = "Map of institutions"
+    url7 = queries.get_query_url_for_citing_authors(
+        format_with_prefix(qids_for_articles)
+    )
     url7_legend = "Authors that cited those works"
 
-
-    license_statement = '''
+    license_statement = """
             This content is available under a <a target="_blank" href="https://creativecommons.org/publicdomain/zero/1.0/"> 
             Creative Commons CC0</a> license.
-    '''
-    scholia_credit_statement = '''
+    """
+    scholia_credit_statement = """
     SPARQL queries adapted from <a target="_blank" href="https://scholia.toolforge.org/">Scholia</a>
-    '''
+    """
 
-    creator_statement = '''
+    creator_statement = """
     Dashboard  generated via Wikidata Bib
-    '''
+    """
 
-
-    html = """
+    html = (
+        """
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>""" + site_title + """</title>
+  <title>"""
+        + site_title
+        + """</title>
   <meta property="og:description" content="powered by Wikidata">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
@@ -70,8 +116,12 @@ def render_dashboard(qids_for_articles, site_title = "Wikidata Bibtex", site_sub
     <div class="container">
       <div class="columns is-centered">
         <div class="column is-half has-text-centered">
-          <h1 class="title is-1"> """ + site_title + """</h1>
-          <h2>""" + site_subtitle +"""</h2>
+          <h1 class="title is-1"> """
+        + site_title
+        + """</h1>
+          <h2>"""
+        + site_subtitle
+        + """</h2>
         </div>
       </div>
     </div>
@@ -79,34 +129,70 @@ def render_dashboard(qids_for_articles, site_title = "Wikidata Bibtex", site_sub
   </section>
    </section>
 
-      <h5 class="title is-5" style="text-align:center;"> """ + url1_legend + """</h5>
+      <h5 class="title is-5" style="text-align:center;"> """
+        + url1_legend
+        + """</h5>
         <p align="center">
-          <iframe width=75% height="400" src=""" + '"'+ url1 +'"' + """></iframe>
+          <iframe width=75% height="400" src="""
+        + '"'
+        + url1
+        + '"'
+        + """></iframe>
         </p>
     <br></br>
-    <h5 class="title is-5" style="text-align:center;">""" + url2_legend + """ </h5>
+    <h5 class="title is-5" style="text-align:center;">"""
+        + url2_legend
+        + """ </h5>
         <p align="center">
-        <iframe width=75%  height="400" src=""" + '"'+ url2 +'"' + """></iframe>
+        <iframe width=75%  height="400" src="""
+        + '"'
+        + url2
+        + '"'
+        + """></iframe>
         </p>
         <br></br>
-      <h5 class="title is-5" style="text-align:center;display:block;"> """ + url4_legend + """</h5>
+      <h5 class="title is-5" style="text-align:center;display:block;"> """
+        + url4_legend
+        + """</h5>
                   <p align="center">
-          <iframe width=75%   height="400" src=""" + '"'+ url4 +'"' + """></iframe>
+          <iframe width=75%   height="400" src="""
+        + '"'
+        + url4
+        + '"'
+        + """></iframe>
           </p>
    <br></br>
-      <h5 class="title is-5" style="text-align:center;"> """ + url5_legend + """  </h5>
+      <h5 class="title is-5" style="text-align:center;"> """
+        + url5_legend
+        + """  </h5>
         <p align="center">
-            <iframe width=75%  height="400" src=""" + '"'+ url5+'"' + """></iframe>
+            <iframe width=75%  height="400" src="""
+        + '"'
+        + url5
+        + '"'
+        + """></iframe>
         </p>
 <br></br>
-      <h5 class="title is-5" style="text-align:center;"> """ + url6_legend + """ </h5>
+      <h5 class="title is-5" style="text-align:center;"> """
+        + url6_legend
+        + """ </h5>
       <p align="center">
-            <iframe width=75%  height="400" src=""" + '"'+ url6 +'"' + """></iframe>
+            <iframe width=75%  height="400" src="""
+        + '"'
+        + url6
+        + '"'
+        + """></iframe>
       </p>
 <br></br>
-      <h5 class="title is-5" style="text-align:center;"> """ + url7_legend + """ </h5>
+      <h5 class="title is-5" style="text-align:center;"> """
+        + url7_legend
+        + """ </h5>
       <p align="center">
-            <iframe width=75%  height="400" src=""" + '"'+ url7 +'"' + """></iframe>
+            <iframe width=75%  height="400" src="""
+        + '"'
+        + url7
+        + '"'
+        + """></iframe>
       </p>
 <br></br>
   </p>
@@ -115,15 +201,21 @@ def render_dashboard(qids_for_articles, site_title = "Wikidata Bibtex", site_sub
   <footer class="footer">
     <div class="container">
       <div class="content has-text-centered">
-        <p>""" + license_statement + """  </p>
-        <p>""" + scholia_credit_statement + """</p>
-        <p>""" + creator_statement + """ </p>
+        <p>"""
+        + license_statement
+        + """  </p>
+        <p>"""
+        + scholia_credit_statement
+        + """</p>
+        <p>"""
+        + creator_statement
+        + """ </p>
       </div>
     </div>
   </footer>
 </body>
 </html>
-  """ 
-    return(html)
-
+  """
+    )
+    return html
 
