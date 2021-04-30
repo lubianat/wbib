34a2fb8f20d2943d6055ae38718f8f73417d14c2
lubianat <tiago.lubiana.alves@usp.br>
2021-04-30 10:33:34 -0300

    code: Move oprtions to comment

--- wbib/render.py
+++ wbib/render.py
@@ -1,27 +1,28 @@
-import queries
+# Example of query_options dictionary:
+# import queries
 
-query_options = {
-    "map of institutions": {
-        "label": "Map of institutions",
-        "query": queries.get_query_url_for_locations(),
-    },
-    "100 most recent articles": {
-        "label": "100 most recent articles",
-        "query": queries.get_query_url_for_articles(),
-    },
-    "list of authors": {
-        "label": "list of authors",
-        "query": queries.get_query_url_for_authors(),
-    },
-    "list of topics": {
-        "label": "list of co-studied topics",
-        "query": queries.get_topics_as_table(),
-    },
-    "list of journals": {
-        "label": "list of co-studied topics",
-        "query": queries.get_query_url_for_venues(),
-    },
-}
+# query_options = {
+#     "map of institutions": {
+#         "label": "Map of institutions",
+#         "query": queries.get_query_url_for_locations(),
+#     },
+#     "100 most recent articles": {
+#         "label": "100 most recent articles",
+#         "query": queries.get_query_url_for_articles(),
+#     },
+#     "list of authors": {
+#         "label": "list of authors",
+#         "query": queries.get_query_url_for_authors(),
+#     },
+#     "list of topics": {
+#         "label": "list of co-studied topics",
+#         "query": queries.get_topics_as_table(),
+#     },
+#     "list of journals": {
+#         "label": "list of co-studied topics",
+#         "query": queries.get_query_url_for_venues(),
+#     },
+# }
 
 
 def render_section(query_name, query_options):
