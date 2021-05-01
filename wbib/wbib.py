474986e191c7ec31343d42a6fe54ecd14ad86e76
lubianat <tiago.lubiana.alves@usp.br>
2021-05-01 18:55:27 -0300

    style: Apply Google's style guide

--- wbib/wbib.py
+++ wbib/wbib.py
@@ -1,34 +1,52 @@
-"""Main module."""
-import os
-import glob
-import urllib.parse
+""" Main functions for the use by end users.
+
+A set of core functions that may be called from wbib.
+Other modules will contain the specific queries and auxiliary functions.
+
+  Typical usage example:
+  
+  qids = ["Q35185544", "Q34555562", "Q21284234"] 
+  render_dashboard(
+    qids_for_articles= qids,
+    site_title="Wikidata Bibtex",
+    site_subtitle="Demonstration")
+"""
+
+
 import pandas as pd
 from wbib import queries
-import unicodedata
 from wikidata2df import wikidata2df
 
 
 def convert_doi_to_qid(list_of_dois):
     """
+  Converts a list of DOI ids to Wikidata QIDs.
+
   Args:
-    dois: A list of dois without prefix (e.g. ["10.3897/RIO.2.E9342", "10.3389/fimmu.2019.02736"])
+    list_of_dois: 
+      A list of dois without prefix. For example:  
+      ["10.3897/RIO.2.E9342", "10.3389/fimmu.2019.02736"]
 
   Returns:
-    result: A dict with two key-value pairs. The "missing" key contains a set with the DOIs not found, 
-    the "qids" key contain a set with the QIDs found on Wikidata. 
+    result: 
+        A dict with two key-value pairs. The "missing" key contains a set
+        with the DOIs not found, the "qids" key contain a set with the 
+        QIDs found on Wikidata. 
   """
+
     formatted_dois = '{ "' + '" "'.join(list_of_dois) + '"}'
     query = f"""SELECT ?id ?item  ?itemLabel
-    WHERE {{
-        {{
-        SELECT ?item ?id WHERE {{
-            VALUES ?unformatted_id {formatted_dois}
-            BIND(UCASE(?unformatted_id) AS ?id)
-            ?item wdt:P356 ?id.
-        }}
-        }}
-        SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}}}
-    """
+  WHERE {{
+      {{
+      SELECT ?item ?id WHERE {{
+          VALUES ?unformatted_id {formatted_dois}
+          BIND(UCASE(?unformatted_id) AS ?id)
+          ?item wdt:P356 ?id.
+      }}
+      }}
+      SERVICE wikibase:label 
+      {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}}}
+  """
 
     query_result = wikidata2df(query)
 
@@ -55,15 +73,18 @@ def render_dashboard(
     qids_for_articles, site_title="Wikidata Bibtex", site_subtitle="Demonstration"
 ):
     """
-    Renders the html for a default dashboard
+    Crafts the html for a default Wikidata-based dashboard.
 
     Args:
-        qids_for_articles: An array of Wikidata Qids (Q[0-9]*) for scholarly works       
-        site_title = The title of the dashboard
-        site_subtitle = An explaining subtitle for the dashboard
+      qids_for_articles: 
+        An array of Wikidata Qids (Q[0-9]*) for scholarly works       
+      site_title:
+       The title of the dashboard
+      site_subtitle:
+       An explaining subtitle for the dashboard
 
     Returns:
-        An html as an string. 
+      An html document as an string. 
 
     """
     url1 = queries.get_query_url_for_articles(format_with_prefix(qids_for_articles))
@@ -82,7 +103,7 @@ def render_dashboard(
     url7_legend = "Authors that cited those works"
 
     license_statement = """
-            This content is available under a <a target="_blank" href="https://creativecommons.org/publicdomain/zero/1.0/"> 
+      This content is available under a <a target="_blank" href="https://creativecommons.org/publicdomain/zero/1.0/"> 
             Creative Commons CC0</a> license.
     """
     scholia_credit_statement = """
@@ -93,129 +114,126 @@ def render_dashboard(
     Dashboard  generated via Wikidata Bib
     """
 
-    html = (
-        """
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>"""
-        + site_title
-        + """</title>
-  <meta property="og:description" content="powered by Wikidata">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-  <link href="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" rel="stylesheet"
-    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-  <section class="section">
-    <div class="container">
-      <div class="columns is-centered">
-        <div class="column is-half has-text-centered">
-          <h1 class="title is-1"> """
-        + site_title
-        + """</h1>
-          <h2>"""
-        + site_subtitle
-        + """</h2>
-        </div>
-      </div>
-    </div>
-    <div class="column is-half has-text-centered">
-  </section>
-   </section>
+    html ="""<!DOCTYPE html>
+      <html>
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>"""
+           + site_title
+          + """</title>
+        <meta property="og:description" content="powered by Wikidata">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      </head>
+      <body>
+        <section class="section">
+          <div class="container">
+            <div class="columns is-centered">
+              <div class="column is-half has-text-centered">
+                <h1 class="title is-1"> """
+              + site_title
+              + """</h1>
+                <h2>"""
+              + site_subtitle
+              + """</h2>
+              </div>
+            </div>
+          </div>
+          <div class="column is-half has-text-centered">
+        </section>
+        </section>
 
-      <h5 class="title is-5" style="text-align:center;"> """
-        + url1_legend
-        + """</h5>
-        <p align="center">
-          <iframe width=75% height="400" src="""
-        + '"'
-        + url1
-        + '"'
-        + """></iframe>
-        </p>
-    <br></br>
-    <h5 class="title is-5" style="text-align:center;">"""
-        + url2_legend
-        + """ </h5>
-        <p align="center">
-        <iframe width=75%  height="400" src="""
-        + '"'
-        + url2
-        + '"'
-        + """></iframe>
-        </p>
+            <h5 class="title is-5" style="text-align:center;"> """
+              + url1_legend
+              + """</h5>
+              <p align="center">
+                <iframe width=75% height="400" src="""
+              + '"'
+              + url1
+              + '"'
+              + """></iframe>
+              </p>
+          <br></br>
+          <h5 class="title is-5" style="text-align:center;">"""
+              + url2_legend
+              + """ </h5>
+              <p align="center">
+              <iframe width=75%  height="400" src="""
+              + '"'
+              + url2
+              + '"'
+              + """></iframe>
+              </p>
+              <br></br>
+            <h5 class="title is-5" style="text-align:center;display:block;"> """
+              + url4_legend
+              + """</h5>
+                        <p align="center">
+                <iframe width=75%   height="400" src="""
+              + '"'
+              + url4
+              + '"'
+              + """></iframe>
+                </p>
         <br></br>
-      <h5 class="title is-5" style="text-align:center;display:block;"> """
-        + url4_legend
-        + """</h5>
-                  <p align="center">
-          <iframe width=75%   height="400" src="""
-        + '"'
-        + url4
-        + '"'
-        + """></iframe>
-          </p>
-   <br></br>
-      <h5 class="title is-5" style="text-align:center;"> """
-        + url5_legend
-        + """  </h5>
-        <p align="center">
-            <iframe width=75%  height="400" src="""
-        + '"'
-        + url5
-        + '"'
-        + """></iframe>
+            <h5 class="title is-5" style="text-align:center;"> """
+              + url5_legend
+              + """  </h5>
+              <p align="center">
+                  <iframe width=75%  height="400" src="""
+              + '"'
+              + url5
+              + '"'
+              + """></iframe>
+              </p>
+      <br></br>
+            <h5 class="title is-5" style="text-align:center;"> """
+              + url6_legend
+              + """ </h5>
+            <p align="center">
+                  <iframe width=75%  height="400" src="""
+              + '"'
+              + url6
+              + '"'
+              + """></iframe>
+            </p>
+      <br></br>
+            <h5 class="title is-5" style="text-align:center;"> """
+              + url7_legend
+              + """ </h5>
+            <p align="center">
+                  <iframe width=75%  height="400" src="""
+              + '"'
+              + url7
+              + '"'
+              + """></iframe>
+            </p>
+      <br></br>
         </p>
-<br></br>
-      <h5 class="title is-5" style="text-align:center;"> """
-        + url6_legend
-        + """ </h5>
-      <p align="center">
-            <iframe width=75%  height="400" src="""
-        + '"'
-        + url6
-        + '"'
-        + """></iframe>
-      </p>
-<br></br>
-      <h5 class="title is-5" style="text-align:center;"> """
-        + url7_legend
-        + """ </h5>
-      <p align="center">
-            <iframe width=75%  height="400" src="""
-        + '"'
-        + url7
-        + '"'
-        + """></iframe>
-      </p>
-<br></br>
-  </p>
-  </div>
- </br>
-  <footer class="footer">
-    <div class="container">
-      <div class="content has-text-centered">
-        <p>"""
-        + license_statement
-        + """  </p>
-        <p>"""
-        + scholia_credit_statement
-        + """</p>
-        <p>"""
-        + creator_statement
-        + """ </p>
-      </div>
-    </div>
-  </footer>
-</body>
-</html>
-  """
-    )
+        </div>
+      </br>
+        <footer class="footer">
+          <div class="container">
+            <div class="content has-text-centered">
+              <p>"""
+              + license_statement
+              + """  </p>
+              <p>"""
+              + scholia_credit_statement
+              + """</p>
+              <p>"""
+              + creator_statement
+              + """ </p>
+            </div>
+          </div>
+        </footer>
+      </body>
+      </html>"""
+    
     return html
 
